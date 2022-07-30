e3a0d5801b8e4603784daf0772f04d9b8dcba9a8
Florian MASTORAKIS <flmastor@e1r3p1.clusters.42paris.fr>
2022-07-30 11:27:23 +0200

    cleaned

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -92,24 +92,23 @@ int	get_next_line(int fd, char **line)
 	int			reader;
 	int BUFFER_SIZE = ft_strlen(*line);
 
-	if (!line || BUFFER_SIZE < 1 || fd < 0)
+	if (!line || BUFFER_SIZE < 1 || fd < 0)						// Ici la gestion des erreurs, on check les paramètres reçus et le BUFFER_SIZE
 		return (-1);
-	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));	// Allocation sur trois lignes pour le respect de la norme
 	if (buff == NULL)
 		return (-1);
-	reader = 1;
-	while (is_present(str, '\n') == 0 && reader != 0)
+	reader = 1;												// Ici on active le reader pour rentrer dans la boucle qui va permettre d'extraire BUFFER_SIZE char dans buff
+	while (is_present(str, '\n') == 0 && reader != 0)		// Ici on vérifie que on ne trouve pas '\n' dans notre
 	{
-		reader = read(fd, buff, BUFFER_SIZE);
-		if (reader == (-1))
-			return (ft_errror(buff));
-		buff[reader] = '\0';
-		str = ft_merge(str, buff);
+		reader = read(fd, buff, BUFFER_SIZE);				// On call la fonction read
+		if (reader == (-1))									// Ici on renvoie -1 et on libère le buff dans la fonction ft_error si problème avec read
+			return (ft_errror(buff));	
+		buff[reader] = '\0';					// Ici bien penser à sécuriser 
+		str = ft_merge(str, buff);				// Appel de la fonction str_join identique à celle de la piscine sauf qu'on free s1 en cas ou on rerentre dans la boucle
 	}
-	free(buff);
-	*line = ft_untiln(str);
-	str = ft_fromn(str);
-	if (reader == 0)
+	free(buff);									//Ici on free le buff dans le cas ou on ne rentre pas dans la boucle (Buff à êtê alloué en mémoire au début de la fonction)
+	*line = ft_untiln(str);						// On vient chercher le début de la str jusqu'à '\n'
+	str = ft_fromn(str);						// On vient donner le reste de buff après '\n' à la static 'str'
+	if (reader == 0)							// Ici la condition de fin 
 		return (0);
-	return (1);
-}
+	return (1);									// Ici la condition de non fin 
